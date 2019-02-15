6588a55fcef6a659eb63b1c1f0fcd1124a9f6f2e
Nativim <42325066+Nativim@users.noreply.github.com>
2019-02-15 20:19:37 +0000

    Add files via upload

--- Plot4.R
+++ Plot4.R
@@ -17,15 +17,15 @@ graph_data[1:1440,"Time"] <- format(graph_data[1:1440,"Time"],"2007-02-01 %H:%M:
 graph_data[1441:2880,"Time"] <- format(graph_data[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 par(mfrow=c(2,2))
 
-plot(graph_data$Time, graph_data$Global_active_power, type = "l", xlab = "", ylab = "Global active Power")
+plot(graph_data$Time, as.numeric(as.character(graph_data$Global_active_power)), type = "l", xlab = "", ylab = "Global active Power")
 
-plot(graph_data$Time, graph_data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+plot(graph_data$Time, as.character(graph_data$Voltage), type = "l", xlab = "datetime", ylab = "Voltage")
 
 plot(graph_data$Time, graph_data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(graph_data$Time, graph_data$Sub_metering_2, col="red")
 lines(graph_data$Time, graph_data$Sub_metering_3, col="blue")
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-plot(graph_data$Time, graph_data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+plot(graph_data$Time, as.character(graph_data$Global_reactive_power), type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 
 dev.off()
